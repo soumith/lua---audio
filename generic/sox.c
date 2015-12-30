beaeb8c8f7ea7069833b773d12aa9030fbf6a3d1
soumith <soumith@fb.com>
2015-12-29 17:40:38 -0800

    fix for older sox versions

--- generic/sox.c
+++ generic/sox.c
@@ -41,7 +41,7 @@ void libsox_(read_audio_file)(const char *file_name, THTensor* tensor, int* samp
   fd = sox_open_read(file_name, NULL, NULL, NULL);
   if (fd == NULL)
     THError("[read_audio_file] Failure to read file");
-  
+
   int nchannels = fd->signal.channels;
   long buffer_size = fd->signal.length;
   *sample_rate = (int) fd->signal.rate;
@@ -49,10 +49,10 @@ void libsox_(read_audio_file)(const char *file_name, THTensor* tensor, int* samp
   size_t samples_read = sox_read(fd, buffer, buffer_size);
   if (samples_read == 0)
     THError("[read_audio_file] Empty file or read failed in sox_read");
-  // alloc tensor 
+  // alloc tensor
   THTensor_(resize2d)(tensor, samples_read / nchannels, nchannels );
   real *tensor_data = THTensor_(data)(tensor);
-  // convert audio to dest tensor 
+  // convert audio to dest tensor
   int x,k;
   for (x=0; x<samples_read/nchannels; x++) {
     for (k=0; k<nchannels; k++) {
@@ -79,14 +79,16 @@ void libsox_(write_audio_file)(const char *file_name, THTensor* src, const char 
   sinfo.channels = nchannels;
   sinfo.length = nsamples * nchannels;
   sinfo.precision = sizeof(int32_t) * 8; /* precision in bits */
+#if SOX_LIB_VERSION_CODE >= 918272 // >= 14.3.0
   sinfo.mult = NULL;
+#endif
   fd = sox_open_write(file_name, &sinfo, NULL, extension, NULL, NULL);
   if (fd == NULL)
     THError("[write_audio_file] Failure to open file for writing");
-  
+
   real* data = THTensor_(data)(src);
 
-  // convert audio to dest tensor 
+  // convert audio to dest tensor
   int x,k;
   for (x=0; x<nsamples; x++) {
     for (k=0; k<nchannels; k++) {
