b9f922a64b2e94c053b10e57c1312ee7ca9a4a3c
Soumith Chintala <soumith@fb.com>
2016-02-03 18:49:11 -0500

    lua 5.3 fixes

--- audio.c
+++ audio.c
@@ -8,6 +8,11 @@
 
 #include <fftw3.h>
 
+#if LUA_VERSION_NUM >= 503
+#define luaL_checklong(L,n)     ((long)luaL_checkinteger(L, (n)))
+#define luaL_checkint(L,n)      ((int)luaL_checkinteger(L, (n)))
+#endif
+
 void abort_(const char * s, ...)
 {
   va_list args;
